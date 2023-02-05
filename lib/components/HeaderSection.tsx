671e7c284f2318d028804cf044a7d9f9fa1b3a20
themuuln <zerone.offical@gmail.com>
2023-02-05 14:55:28 +0800

    first

---
+++ lib/components/HeaderSection.tsx
@@ -0,0 +1,13 @@
+import { motion } from "framer-motion";
+
+function HeaderSection() {
+  return (
+    <>
+      <header>
+        <motion.text>Throw the Dice!</motion.text>
+      </header>
+    </>
+  );
+}
+
+export default HeaderSection;
